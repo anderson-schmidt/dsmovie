99e1b5bf87fa1228bb85af43553eec8b73fe6b56
sorinmarkov <anderson.schmidt90@gmail.com>
2022-01-10 15:54:00 -0300

    navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,34 @@
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg'
+import './styles.css';
+
+function Navbar() {
+
+    return (
+
+        <header>
+            <nav className="container">
+
+                <div className="dsmovie-nav-content">
+
+                    <h1>DSMovie</h1>
+
+                    <a href="https://github.com/sorinmarkov">
+
+                        <div className="dsmove-contact-container">
+
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link">/schmidtmarkov</p>
+
+                        </div>
+                    </a>
+
+                </div>
+
+            </nav>
+        </header>
+
+    );
+
+}
+
+export default Navbar;
